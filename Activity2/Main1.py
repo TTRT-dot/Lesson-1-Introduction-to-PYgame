3040e490b80cf1eb81257c818795d982458f03ac
TTRT-dot <xdamer6469@gmail.com>
2025-07-06 14:26:49 +0530

    asd

---
+++ Activity2/Main1.py
@@ -0,0 +1,40 @@
+#import and initialize pygame
+import pygame
+pygame.init()
+white = (255,255,255)
+blue = (0,0,255)
+
+#Clock
+clock = pygame.time.Clock()
+
+#creating the display surface object
+#or specific dimention...e(X,Y)
+display_surface=pygame.display.set_mode((1940,1020))
+
+#set the pygame widnow name
+pygame.display.set_caption("IIIIMMMMAAAAGGGEEEEEEEE")
+
+#creating a surface object, image is drawn on it.
+image=pygame.image.load('Football.png')
+
+#Set the size for the image
+DEFAULT_IMAGE_SIZE=(900,900)
+
+#Scale the image to your needed size
+image = pygame.transform.scale(image,DEFAULT_IMAGE_SIZE)
+
+#Set a default position
+DEFAULT_IMAGE_POSITION=(1000,10)
+
+#infinate loop
+while True:
+    display_surface.fill(white)
+    display_surface.blit(image,DEFAULT_IMAGE_POSITION)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+
+    pygame.display.flip()
+    clock.tick(30)
