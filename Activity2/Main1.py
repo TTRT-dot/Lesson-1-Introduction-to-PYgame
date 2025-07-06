90c5c25f9972aede9c101019a0dfe839ed2b54fa
TTRT-dot <xdamer6469@gmail.com>
2025-07-06 14:32:52 +0530

    das

--- Activity2/Main1.py
+++ Activity2/Main1.py
@@ -12,24 +12,25 @@ clock = pygame.time.Clock()
 display_surface=pygame.display.set_mode((1940,1020))
 
 #set the pygame widnow name
-pygame.display.set_caption("IIIIMMMMAAAAGGGEEEEEEEE")
+pygame.display.set_caption("Football Images")
 
 #creating a surface object, image is drawn on it.
 image=pygame.image.load('Football.png')
-
+image1=pygame.image.load('fireball.png')
 #Set the size for the image
 DEFAULT_IMAGE_SIZE=(900,900)
 
 #Scale the image to your needed size
 image = pygame.transform.scale(image,DEFAULT_IMAGE_SIZE)
-
+image1 = pygame.transform.scale(image1,DEFAULT_IMAGE_SIZE)
 #Set a default position
 DEFAULT_IMAGE_POSITION=(1000,10)
-
+DEFAULT_IMAGE1_POSITION=(10,10)
 #infinate loop
 while True:
     display_surface.fill(white)
     display_surface.blit(image,DEFAULT_IMAGE_POSITION)
+    display_surface.blit(image1,DEFAULT_IMAGE1_POSITION)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
